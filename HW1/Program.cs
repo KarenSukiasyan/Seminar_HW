2edb965edc453f9ac5f6d550faf3c63e689573a3
Karen Sukiasyan <armenfilm@mail.ru>
2022-08-15 14:39:52 +0300

    Task 1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+Console.WriteLine(num1);
+else
+    if (num2 > num1)
+        Console.WriteLine(num2);
+
