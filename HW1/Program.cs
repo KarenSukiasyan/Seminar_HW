d379d1b41b7e2fd88a52fb011a3834e82ddb888e
KarenSukiasyan <110194901+KarenSukiasyan@users.noreply.github.com>
2022-08-15 22:44:34 +0300

    Update Program.cs

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -77,6 +77,26 @@ else
         Console.WriteLine(num2);
 
 
+// Напишите пограмму которая принимет на вход три числа и выдаёт максимальное.
+
+Console.Write("Input number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (max < number2)
+{
+    max = number2;
+}
+if (max < number3)
+{
+    max = number3;
+}
+
+Console.WriteLine($"maks number = {max}");
 
 
 
