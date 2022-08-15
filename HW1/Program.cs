6b6e7d519c0b8aae2e67b97f88d6d9bf773fbfb0
Karen Sukiasyan <armenfilm@mail.ru>
2022-08-15 22:08:21 +0300

    HW1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,4 +1,5 @@
-﻿// Написать программу, которая на вход принимает три числа и выдаёт максимальное из этих чисел.
+﻿// Написать программу, которая на вход принимает три числа и 
+// выдаёт максимальное из этих чисел.
 
 Console.Clear();
 Console.Write("Input number1: ");
@@ -19,6 +20,48 @@ if (max < number3)
 }
 
 Console.WriteLine($"maks number = {max}");
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число четным.
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2 == 0)
+{
+    Console.WriteLine("Even number");
+}
+else
+{
+    Console.WriteLine("Odd number");
+}
+
+// Напишите программу, которая на вход принимает одно число(N), а на выходе
+//  показывает все целые числа от -N до N.
+
+Console.Write("Input number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int B = -N;
+while (B <= N)
+{
+    Console.Write(B+ " ");
+    B++;
+}
+
+
+// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Input three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99 && num <= 999)
+{
+    Console.Write("Last number: " + Math.Abs(num) % 10);
+}
+else 
+{
+    Console.WriteLine("The entered number is not three digits");
+}
 
 
 
