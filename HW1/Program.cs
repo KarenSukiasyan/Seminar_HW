aec3392a9876af04ef446b7db768b0d933666525
KarenSukiasyan <110194901+KarenSukiasyan@users.noreply.github.com>
2022-08-15 19:09:42 +0300

    Update Program.cs

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,4 +1,17 @@
-﻿// Написать программу, которая на вход принимает три числа и выдаёт максимальное из этих чисел.
+// Написать программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+Console.WriteLine(num1);
+else
+    if (num2 > num1)
+        Console.WriteLine(num2);
+
+// Написать программу, которая на вход принимает три числа и выдаёт максимальное из этих чисел.
 
 Console.Clear();
 Console.Write("Input number1: ");
